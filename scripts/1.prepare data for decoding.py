36b7ee2f91af06ea39460873c0284e8a21a1a4d2
nmningmei <semsociaty@gmail.com>
2022-02-21 11:40:20 +0100

    update

--- scripts/1.prepare data for decoding.py
+++ scripts/1.prepare data for decoding.py
@@ -43,7 +43,8 @@ for experiment,target_column in zip(['confidence','accuracy','confidence-accurac
         n_jobs              = -1
         verbose             = 1
         
-        df_def              = preprocess(working_data,target_columns = target_columns,n_jobs = n_jobs)
-        if not os.path.exists(os.path.join(data_dir,f'{experiment}')):
-            os.makedirs(os.path.join(data_dir,f'{experiment}'))
-        df_def.to_csv(working_df_name,index = False)
+        if 'RT' not in experiment and working_dir != 'cognitive-4-rating':
+            df_def              = preprocess(working_data,target_columns = target_columns,n_jobs = n_jobs)
+            if not os.path.exists(os.path.join(data_dir,f'{experiment}')):
+                os.makedirs(os.path.join(data_dir,f'{experiment}'))
+            df_def.to_csv(working_df_name,index = False)
